523abc548a40988b554ea67edd386362d2d623d8
aryanjain18 <jain.aryan00007@gmail.com>
2022-10-18 20:25:35 +0530

    Initial Project

---
+++ 1_HelloWorld.py
@@ -0,0 +1,4 @@
+#Next Commmand Prints String Hello World
+print("Hello World")
+#Next Command Prints Number 2
+print(2)
---
+++ 3_ConvertVariable.py
@@ -0,0 +1,7 @@
+w = 2.2
+x = 2.7
+print(int(x))
+print(int(w))
+y = str(x)
+print(type(x))
+print(type(y))

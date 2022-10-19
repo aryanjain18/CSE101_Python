924b6c5ea93b64b102c7ccc099fea36a65a0c8f8
aryanjain18 <jain.aryan00007@gmail.com>
2022-10-19 13:26:40 +0530

    19.10.2022

---
+++ 4_NumberOperators.py
@@ -0,0 +1,17 @@
+x = 3
+y = 2
+#Addition
+a = x + y
+#Subtraction
+s = x - y
+#Multiplication
+m = x * y
+#Division
+d = x / y
+#Floor Division - Quotient
+q = x // y
+#Reminder - Modulo
+r = x % y
+#Exponentiation
+e = x ** y 
+print(a,s,m,d,q,r,e)
---
+++ 6_ComparisionOperators.py
@@ -0,0 +1,10 @@
+x = 2
+y = 3
+print(x==y) #Equal To
+print(x!=y) #Not Equal To
+print(x>y) #GreaterThan
+print(x<y) #LessThan 
+print(x>=y) #GreaterThanEqualTo
+print(x<=y) #LessThanEqualTo
+
+print(x <= y-1) 

138e2bef7077a275feb6b4f7aa5665a5d62dc657
aryanjain18 <jain.aryan00007@gmail.com>
2022-10-19 21:35:07 +0530

    Operators Added

--- 4_NumberOperators.py
+++ 4_NumberOperators.py
@@ -15,3 +15,37 @@ r = x % y
 #Exponentiation
 e = x ** y 
 print(a,s,m,d,q,r,e)
+
+#COMPARISION OPERATORS
+x = 2
+y = 3
+print(x==y) #Equal To
+print(x!=y) #Not Equal To
+print(x>y) #GreaterThan
+print(x<y) #LessThan 
+print(x>=y) #GreaterThanEqualTo
+print(x<=y) #LessThanEqualTo
+
+print(x <= y-1) 
+
+#ASSIGNMENT OPERATORS
+x = 2
+print(x)
+x = x + 2
+print(x)
+x+=2
+print(x)
+x-=3 #x = x-3
+print(x)
+x = x*3
+print(x)
+x*=3
+print(x)
+x = x/3
+print(x)
+x/=3
+print(x)
+x**=3 #x = x ** 3
+print(x)
+x//=4 #x = x // 4
+print(x)
---
+++ 5_ExamplesOperators.py
@@ -0,0 +1,25 @@
+#Example 1
+#BDMAS Test
+a = 24/2-6
+b = 24-6/2
+c = (3+1)*(4+4)/(1+1)
+print(a,b,c)
+
+#Example 2
+#Find number of divisiors of 360?
+#Ans. Prime Factorisation Of 360 = 2*2*2*3*3*5
+#360 = 2^3 * 3^2 * 5^1
+n = (3+1)*(2+1)*(1+1)
+print(n)
+
+#Example 3
+# Find e^pi v/s pi^e?
+import math
+t = math.e ** math.pi
+p = math.pi ** math.e
+print("e^pi > pi^e ? ", t > p)
+print(t,p)
+print("e > pi ? ", math.e > math.pi)
+
+#Example 4
+#1. Exchange values inside variables using a dummy variable.

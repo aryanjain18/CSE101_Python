7fa28ef84669340d541ec1937bd9240d9a4325e0
aryanjain18 <jain.aryan00007@gmail.com>
2022-11-27 13:32:10 +0530

    Jupyter Notebooks

--- 1_HelloWorld.py
+++
@@ -1,4 +0,0 @@
-#Next Commmand Prints String Hello World
-print("Hello World") #Print = Function. Prints Whatever Is Inside Brackets
-#Next Command Prints Number 2
-print(2)
--- 3_ConvertVariable.py
+++
@@ -1,7 +0,0 @@
-w = 2.2
-x = 2.7
-print(int(x))
-print(int(w))
-y = str(x)
-print(type(x))
-print(type(y))
